4a16a22d2bf5b46e8e663a55b62b56a48c554e07
Dan Allen <dan@opendevise.com>
2018-09-10 22:08:09 -0600

    move UI download to script

---
+++ download-ui-bundle.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl -s -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/mulesoft/docs-site-antora-ui/releases \
+  | tr '\n' ' ' | grep -oP '"assets":.*?"id":[^,]+' | head -1 | grep -oP '[0-9]+$' \
+  | curl -sL -o build/ui-bundle.zip --create-dirs -H "Accept: application/octet-stream" https://api.github.com/repos/mulesoft/docs-site-antora-ui/releases/assets/$(cat /dev/stdin)?access_token=$GITHUB_TOKEN
